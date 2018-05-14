ebee4569136d58152dc7d0f5d617b4eddc7fa810
eliam lotonga <eliam lotonga>
2018-05-14 22:31:54 +0200

    Correction methode telechargement ICs

--- app/src/main/java/lotonga/e/dev/ives/fr/mmi/TelechargerFichierICS/RepeatDownload.java
+++ app/src/main/java/lotonga/e/dev/ives/fr/mmi/TelechargerFichierICS/RepeatDownload.java
@@ -1,18 +1,35 @@
 package lotonga.e.dev.ives.fr.mmi.TelechargerFichierICS;
 
 import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class RepeatDownload {
 
+    private static final long RAFRAICHISSEMENT = 1000 * 60 * 2; //2 minutes
     private final static int INTERVAL = 1000 * 60 * 2; //2 minutes
-    Handler mHandler = new Handler();
+    Handler mHandler = new Handler() {
+        @Override
+        public void publish(LogRecord record) {
+
+        }
+
+        @Override
+        public void flush() {
+
+        }
+
+        @Override
+        public void close() throws SecurityException {
+
+        }
+    };
 
     Runnable mHandlerTask = new Runnable()
     {
         @Override
         public void run() {
             TelechargerDepuisURL.getContentTO("");
-            mHandler.postDelayed(mHandlerTask, INTERVAL);
+            //mHandler.postDelayed(mHandlerTask, INTERVAL);
         }
     };
 
--- app/src/main/java/lotonga/e/dev/ives/fr/mmi/TelechargerFichierICS/TelechargerDepuisURL.java
+++ app/src/main/java/lotonga/e/dev/ives/fr/mmi/TelechargerFichierICS/TelechargerDepuisURL.java
@@ -10,16 +10,62 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 public class TelechargerDepuisURL {
 
-    public static BufferedReader getContentTO(String url)
+    private static String base_url= "http://ade6-ujf-ro.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=5946,5945,5944,4592,7357,7836,7350,7335,7334,7326,4907,4898,7385,7384,7317,7265,7390,7389,7387,7380,7395,7394,5055,5057,5029,5028,5024,5023,7373,7369,7367,7364,2650,7360,7377,7396,7375,7338,7399,7398&projectId=2&calType=ical&";
+
+    private static String getCurrentDate()
     {
+        int year, month, day;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            LocalDateTime date = LocalDateTime.now();
+            year = date.getYear();
+            month = date.getMonthValue();
+            day = date.getDayOfWeek().getValue();
+            //ZonedDateTime zdt = ZonedDateTime.now();
+        }
+        else
+        {
+
+            Date date = new Date();
+            year = date.getYear();
+            month = date.getMonth();
+            day = date.getDay();
+        }
+        String ch = String.format("%d-%d-%d",year,month, day );
+        return ch;
+    }
+
+    public static BufferedReader getContent(String dateFin)
+    {
+        base_url += "firstDate="+getCurrentDate()+"&lastDate="+dateFin;
         try
         {
             HttpClient client = HttpClientBuilder.create().build();
-            HttpGet request = new HttpGet(url);
-
+            HttpGet request = new HttpGet(base_url);
+            // add request header
+            //request.addHeader("User-Agent", USER_AGENT);
+            HttpResponse response = client.execute(request);
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            return rd;
+        }
+        catch(Exception e)
+        {}
+        return null;
+    }
+    public static BufferedReader getContentTO(String dateFin)
+    {
+        try
+        {
+            base_url += "firstDate="+getCurrentDate()+"&lastDate="+dateFin;
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(base_url);
             // add request header
             //request.addHeader("User-Agent", USER_AGENT);
             HttpResponse response = client.execute(request);
@@ -33,11 +79,12 @@ public class TelechargerDepuisURL {
         return null;
     }
 
-    public static boolean TelechargerFichier(String url, String destination)
+    public static boolean TelechargerFichier( String destination, String dateFin )
     {
         try{
+            base_url += "firstDate="+getCurrentDate()+"&lastDate="+dateFin;
             HttpClient client = HttpClientBuilder.create().build();
-            HttpGet request = new HttpGet(url);
+            HttpGet request = new HttpGet(base_url);
             // add request header
             //request.addHeader("User-Agent", USER_AGENT);
             HttpResponse response = client.execute(request);
