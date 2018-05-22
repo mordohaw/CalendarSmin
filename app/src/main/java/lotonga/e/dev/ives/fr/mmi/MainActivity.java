4cad909469b8ef05c0527e35d0c5da50f131c1e2
LeoMedel <leonel.ilhuicatzi07@gmail.com>
2018-05-22 15:31:13 +0200

    feat methode telechargerment du ADE

--- app/src/main/java/lotonga/e/dev/ives/fr/mmi/MainActivity.java
+++ app/src/main/java/lotonga/e/dev/ives/fr/mmi/MainActivity.java
@@ -1,13 +1,126 @@
 package lotonga.e.dev.ives.fr.mmi;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.ComponentList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import lotonga.e.dev.ives.fr.mmi.TelechargerFichierICS.TelechargerDepuisURL;
 
 public class MainActivity extends Activity {
+
+    Button btn;
+
+    protected boolean shouldAskPermissions() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    @TargetApi(23)
+    protected void askPermissions() {
+        String[] permissions = {
+                "android.permission.READ_EXTERNAL_STORAGE",
+                "android.permission.WRITE_EXTERNAL_STORAGE"
+        };
+        int requestCode = 200;
+        requestPermissions(permissions, requestCode);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (shouldAskPermissions()) {
+            askPermissions();
+        }
+
+        btn = (Button) findViewById(R.id.telecharge);
+    }
+
+    public void click(View view)
+    {
+        String urlADEics = "http://ade6-ujf-ro.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=7265&projectId=2&calType=ical&firstDate=2018-05-13&lastDate=2018-05-18";
+
+        new TelechargeADE().execute(urlADEics);
+    }
+
+
+    public class TelechargeADE extends AsyncTask<String, String, String>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urlADE) {
+
+            String url = urlADE[0];
+            File destination = new File(Environment.getExternalStorageDirectory(), "ADE.ics");
+
+            try{
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpGet request = new HttpGet(url);
+                HttpResponse response = client.execute(request);
+
+                String line = "";
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(destination), "utf-8") );
+
+                while ((line = rd.readLine()) != null) {
+
+                    writer.append(line);
+                    writer.newLine();
+
+                }
+                writer.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                return "Download Error !";
+            }
+            return "Download Succes";
+        }
+
+        @Override
+        protected void onProgressUpdate(String ... value) {
+            super.onProgressUpdate(value);
+        }
+
+        @Override
+        protected void onPostExecute(String message) {
+            super.onPostExecute(message);
+
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/lotonga/e/dev/ives/fr/mmi/TelechargerFichierICS/TelechargerDepuisURL.java
+++ app/src/main/java/lotonga/e/dev/ives/fr/mmi/TelechargerFichierICS/TelechargerDepuisURL.java
@@ -1,5 +1,9 @@
 package lotonga.e.dev.ives.fr.mmi.TelechargerFichierICS;
 
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.widget.Toast;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -7,6 +11,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -15,7 +20,10 @@ import java.util.Date;
 
 public class TelechargerDepuisURL {
 
+
     private static String base_url= "http://ade6-ujf-ro.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=5946,5945,5944,4592,7357,7836,7350,7335,7334,7326,4907,4898,7385,7384,7317,7265,7390,7389,7387,7380,7395,7394,5055,5057,5029,5028,5024,5023,7373,7369,7367,7364,2650,7360,7377,7396,7375,7338,7399,7398&projectId=2&calType=ical&";
+    //private static String destination = "";
+    private static String reponseDowload = "";
 
     private static String getCurrentDate()
     {
@@ -39,6 +47,11 @@ public class TelechargerDepuisURL {
         return ch;
     }
 
+    /**
+     *
+     * @param dateFin => format yyyy-mm-jj
+     * @return
+     */
     public static BufferedReader getContent(String dateFin)
     {
         base_url += "firstDate="+getCurrentDate()+"&lastDate="+dateFin;
@@ -61,31 +74,63 @@ public class TelechargerDepuisURL {
         return null;
     }
 
-
-    public static boolean TelechargerFichier( String destination, String dateFin )
+    public static String TelechargerFichier()
     {
-        try{
-            base_url += "firstDate="+getCurrentDate()+"&lastDate="+dateFin;
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpGet request = new HttpGet(base_url);
-            // add request header
-            //request.addHeader("User-Agent", USER_AGENT);
-            HttpResponse response = client.execute(request);
-            //System.out.println("Response Code : "+ response.getStatusLine().getStatusCode());
-            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String line = "";
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(destination), "utf-8") );
-            while ((line = rd.readLine()) != null) {
-                //System.out.println(line);
-                writer.append(line);
-                writer.newLine();
-            }
-            writer.close();
-            return true;
+        String urlADEics = "http://ade6-ujf-ro.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=7265&projectId=2&calType=ical&firstDate=2018-05-13&lastDate=2018-05-18";
+
+        new TelechargeADE().execute(urlADEics);
+
+        return reponseDowload;
+    }
+
+
+    static class TelechargeADE extends AsyncTask<String, Integer, String>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
+
+        @Override
+        protected String doInBackground(String... urlADE) {
+
+            String url = urlADE[0];
+            File destination = new File(Environment.getExternalStorageDirectory(), "ADE.ics");
+
+            try{
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpGet request = new HttpGet(url);
+                HttpResponse response = client.execute(request);
+
+                String line = "";
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(destination), "utf-8") );
+
+                while ((line = rd.readLine()) != null) {
+
+                    writer.append(line);
+                    writer.newLine();
+
+                }
+                writer.close();
+            }
             catch(Exception e)
-        {
-            return false;
+            {
+                e.printStackTrace();
+                return "Download Error !";
+            }
+            return "Download Succes";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... value) {
+            super.onProgressUpdate(value);
+        }
+
+        @Override
+        protected void onPostExecute(String message) {
+            super.onPostExecute(message);
+            reponseDowload = message;
         }
     }
 
